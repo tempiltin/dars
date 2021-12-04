736691b657769c407c360951acfd5a3a2c452e81
Tempiltin99 <temurbekshukurov0707@gmail.com>
2021-12-04 17:09:59 +0500

    upload

--- script/clock.js
+++ script/clock.js
@@ -1,34 +1,17 @@
-function countdown (){
-    var now = new Date();
-    var eventDate = new Date(2021, 12, 4 );
-
-    var currentTime = now.getTime();
-    var eventTime = eventDate.getTime();
-
-    var remTime = eventTime -currentTime;
-
-    var s = Math.floor(remTime / 1000);
-    var m = Math.floor(s / 60);
-    var h = Math.floor(m / 24);
-    var d = Math.floor(h / 24);
-
-    h %= 24;
-    m %= 60;
-    s %= 60;
-
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-     
-
-    document.getElementById("days").textContent = d;
-    document.getElementById("days").innerText = d;
+const lang = navigator.language;
+let date = new Date();
 
 
-    document.getElementById("hours").textContent = h;
-    document.getElementById("minuts").textContent = m;
-    document.getElementById("secouds").textContent = s;
-    setTimeout(countdown , 1000);
+let dayNumber = date.getDate();
+let month =  date.getMonth();
+let dayName = date.toLocaleString(lang , {weekday: 'long'});
+let monthName = date.toLocaleString(lang , {month: 'long'});
 
-}
-countdown()
+let year = date.getFullYear();
+
+document.getElementById('monthName').innerHTML = monthName;
+document.getElementById('dayName').innerHTML = dayName;
+document.getElementById('dayNumber').innerHTML = dayNumber;
+document.getElementById('year').innerHTML = year;
+
+
